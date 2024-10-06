fc0bae96e665a47101678246bcecd2b98a5e1f2c
Taisuke Fukuno <fukuno@jig.jp>
2024-10-06 22:04:08 +0900

    add CHAR param

--- hex2bas.js
+++ hex2bas.js
@@ -1,5 +1,5 @@
-export const hex2bas = (s, hex = true) => {
-  const res = ["POKE#700"];
+export const hex2bas = (s, hex = true, firstch = 224) => {
+  const res = ["POKE#" + (firstch * 8).toString(16).toUpperCase()];
   const hexordec = (s) => {
     if (hex) return "#" + s;
     return "" + parseInt(s, 16);
@@ -8,12 +8,13 @@ export const hex2bas = (s, hex = true) => {
     for (let i = 0; i < 8; i++) {
       res.push("," + hexordec(s.substring(i * 2, i * 2 + 2)));
     }
-    res.push(":?CHR$(224)");
+    res.push(`:?CHR$(${firstch})`);
   } else if (s.length == 16 * 4) {
     for (let i = 0; i < 8 * 4; i++) {
       res.push("," + hexordec(s.substring(i * 2, i * 2 + 2)));
     }
-    res.push(":?CHR$(224,225,28,28,31,226,227)");
+    const c = parseInt(firstch);
+    res.push(`:?CHR$(${c},${c + 1},28,28,31,${c + 2},${c + 3})`);
   } else {
     throw new Error("not supported size");
   }
